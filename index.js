758e4d0d59750df40bdb11a09c4d3da278c71722
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-07 14:26:48 +0000

    Second Commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: [
+  Discord.GatewayIntentBits.Guilds,
+  Discord.GatewayIntentBits.GuildMessages
+]})
 const token = process.env.DISCORD_BOT_SECRET;
 
 client.on('ready', () => {
