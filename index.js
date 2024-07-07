716f9ce47edb759add63dd90860b17cd12dcda4f
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-07 17:40:10 +0000

    Fourth Commit

--- index.js
+++ index.js
@@ -22,4 +22,34 @@ client.on("messageCreate", message => {
     }
 })
 
+client.on("messageCreate", message => {
+    if(message.content === "!hackclub") {
+    message.channel.send("hackclub is a non-profit from vermont, usa that hosts events based on coding, engineering and all sorts of things!")
+    }
+})
+
+client.on("messageCreate", message => {
+    if(message.content === "cat") {
+    message.channel.send("meow :3")
+    }
+})
+
+client.on("messageCreate", message => {
+    if(message.content === "!help") {
+    message.channel.send("dm me for questions")
+    }
+})
+
+client.on("messageCreate", message => {
+    if(message.content === "stackoverflow") {
+    message.channel.send("stackoverflow.com")
+    }
+})
+
+client.on("messageCreate", message => {
+    if(message.content === "!lol") {
+    message.channel.send("yay!")
+    }
+})
+
 client.login(token);
